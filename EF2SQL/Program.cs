9e098ce877a28d8b0585b103847b908d710ac58f
Lidya Tsegay <lidyatsegay@outlook.com>
2019-09-11 14:26:40 -0400

    updating and making changes in the SQL database using EF

--- EF2SQL/Program.cs
+++ EF2SQL/Program.cs
@@ -9,13 +9,50 @@ namespace EF2SQL {
             //Create instance of the context
             using (var context = new PrsDBContext()) {
 
+                //update a request into SQL table
+                //var request = new Requests() {
+                //    Id = 0,
+                //    Description = "One More Time",
+                //    Justification = "Just for fun!",
+                //    RejectionReason = null,
+                //    DiliveryMode = "Pickup",
+                //    Status = "New",
+                //    Total = 23,
+                //    UserId = context.Users.SingleOrDefault(u => u.Username.Equals("ABC789")).Id
+                //};
+                //context.Requests.Add(request); // this does not update the database, it updates the collection
+                //context.SaveChanges(); // this saves it into the database
+
+                //update a column for a request
+                //var request = context.Requests.Find(3); // read the record
+                //request.Total = 71; //make the change
+                //context.SaveChanges();
+
+
+                //updating info: changes made by a user to update the database
+                var request = new Requests() { Id = 3, Description = "New New Request" };
+                var dbRequest = context.Requests.Find(request.Id);
+                dbRequest.Description = request.Description;
+                context.SaveChanges();
+
+
                 //first thing you reference is the "context" instance 
-                var users = context.Users.ToList();
+                var bruetta = context.Vendors.Find(5);
+                Console.WriteLine($"{bruetta.Code} {bruetta.Name}") ;
+
+                //var users = context.Users.ToList();
+                //users.ForEach(u => Console.WriteLine($"{u.Username} {u.Firstname } {user.LastName}")); better way to do the foreach below
+
+                var users = from u in context.Users.ToList()
+                            where u.Username.Contains("5") || u.Username.Contains("8")
+                            select u;
+
                 foreach (var user in users) {
                     Console.WriteLine($"{user.Username} {user.FirstName} {user.LastName}");
                 }
 
-            }
+
+            } 
 
         }
     }
--- EF2SQLLibrary/PrsDBContext.cs
+++ EF2SQLLibrary/PrsDBContext.cs
@@ -25,7 +25,7 @@ namespace EF2SQLLibrary
         {
             if (!optionsBuilder.IsConfigured)
             {
-#warning To protect potentially sensitive information in your connection string, you should move it out of source code. See http://go.microsoft.com/fwlink/?LinkId=723263 for guidance on storing connection strings.
+                optionsBuilder.UseLazyLoadingProxies(); //optional, must put in yourself
                 optionsBuilder.UseSqlServer("server=localhost\\sqlexpress;database=PrsDB;trusted_connection=true;");
             }
         }
