4d325f9c1aca130d4c70341c2dfeadbd0f691bf8
Lidya Tsegay <lidyatsegay@outlook.com>
2019-09-11 16:37:49 -0400

    User Repository Created Exceptions

--- EF2SQL/Program.cs
+++ EF2SQL/Program.cs
@@ -9,6 +9,7 @@ namespace EF2SQL {
             //Create instance of the context
             using (var context = new PrsDBContext()) {
 
+                #region Examples
                 //update a request into SQL table
                 //var request = new Requests() {
                 //    Id = 0,
@@ -24,35 +25,73 @@ namespace EF2SQL {
                 //context.SaveChanges(); // this saves it into the database
 
                 //update a column for a request
-                //var request = context.Requests.Find(3); // read the record
-                //request.Total = 71; //make the change
+                //var request = context.Requests.Find(2); // read the record
+                //request.Total = 200; //make the change
                 //context.SaveChanges();
 
 
                 //updating info: changes made by a user to update the database
-                var request = new Requests() { Id = 3, Description = "New New Request" };
-                var dbRequest = context.Requests.Find(request.Id);
-                dbRequest.Description = request.Description;
-                context.SaveChanges();
+                //var request = new Requests() { Id = 3, Description = "New New Request" };
+                //var dbRequest = context.Requests.Find(request.Id);
+                //dbRequest.Description = request.Description;
+                //context.SaveChanges();
+
+
+                //Delete a record in sql using EF
+                //var request = new Requests() { Id = 3, Description = "New New Request" };
+                //var dbRequest = context.Requests.Find(request.Id);
+                //dbRequest.Description = request.Description;
+                //context.Requests.Remove(dbRequest);
+                //context.SaveChanges();
 
 
                 //first thing you reference is the "context" instance 
-                var bruetta = context.Vendors.Find(5);
-                Console.WriteLine($"{bruetta.Code} {bruetta.Name}") ;
+                //var bruetta = context.Vendors.Find(5);
+                //Console.WriteLine($"{bruetta.Code} {bruetta.Name}") ;
 
                 //var users = context.Users.ToList();
                 //users.ForEach(u => Console.WriteLine($"{u.Username} {u.Firstname } {user.LastName}")); better way to do the foreach below
 
-                var users = from u in context.Users.ToList()
-                            where u.Username.Contains("5") || u.Username.Contains("8")
-                            select u;
+                //Read for a request /use the ID/ that has lines in it/print of rquest and 
+                //all lines in request (description status total) and line item 
 
-                foreach (var user in users) {
-                    Console.WriteLine($"{user.Username} {user.FirstName} {user.LastName}");
+                //var request3 = context.Requests.Find(2);
+                //Console.WriteLine($"{request3.Description} {request3.Status} {request3.Total.ToString("C")}");
+
+                //Multiple line on a request line
+                //request3.Total = request3.RequestLines.Sum(1 => 1.Product.Price * 1.Quantity);
+
+                //request3.RequestLines.ToList().ForEach(request1 => {
+                //    Console.WriteLine($"{request1.Product.Name,-10} {request1.Quantity,5} " +
+                //        $"{request1.Product.Price.ToString("C"),10} " +
+                //        $"{(request1.Product.Price * request1.Quantity).ToString("C"),11}");
+                //});
+                #endregion
+
+                //Tally up grand total of all request
+                //var reqs = (from r in context.Requests
+                //            select r).ToList();
+                //reqs.ForEach(r => {
+                //    r.Total = r.RequestLines.Sum();
+                //    ToConsole(r)});
+
+                //context.SaveChanges();
+
+                var total = context.Requests.Sum(r => r.Total);
+                    Console.WriteLine($"Total of all request is {total}");
+
+
+                    #region ToList Example
+                    //var users = from u in context.Users.ToList()
+                    //            where u.Username.Contains("5") || u.Username.Contains("8")
+                    //            select u;
+
+                    //foreach (var user in users) {
+                    //    Console.WriteLine($"{user.Username} {user.FirstName} {user.LastName}");
+                    //}
+                    #endregion
+
                 }
-
-
-            } 
 
         }
     }
---
+++ EF2SQLLibrary/UsersRepository.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace EF2SQLLibrary {
+    public class UsersRepository {
+
+        private static PrsDBContext context = new PrsDBContext();
+
+        public static List<Users> GetAll() {
+            return context.Users.ToList();
+        }
+
+        public static Users GetByPk(int id) {
+            return context.Users.Find(id);
+        }
+
+        public static bool Insert(Users user) {
+            if (user == null) { throw new Exception("User instant must not be null"); }
+            user.Id = 0;
+            context.Users.Add(user);
+            return context.SaveChanges() == 1;
+        }
+
+        public static bool Update(Users user) {
+            if (user == null) { throw new Exception("User instant must not be null"); }
+            var dbuser = context.Users.Find(user.Id);
+            if (dbuser == null) { throw new Exception("No user with that ID"); }
+            dbuser.Username = user.Username;
+            dbuser.Password = dbuser.Password;
+            //do the rest of the colum names
+            return context.SaveChanges() == 1;
+        }
+
+        public static bool Delete(Users user) {
+            if (user == null) { throw new Exception("User instant must not be null"); }
+            var dbuser = context.Users.Find(user.Id);
+            if (dbuser == null) { throw new Exception("No user with that ID"); }
+            context.Users.Remove(dbuser);
+            return context.SaveChanges() == 1;
+        }
+        public static bool Delete(int id) {
+            var user = context.Users.Find(id);
+            if(user == null) { return false; }
+            var rc = Delete(user);
+            return context.SaveChanges() == 1;
+        }
+    }
+}
